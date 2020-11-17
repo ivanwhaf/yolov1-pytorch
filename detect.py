fix loss calculation bugs

detect.py
@@ -1,9 +1,11 @@
+import os
+import random
 import argparse
 import cv2
 from torchvision import transforms
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
-from utils.util import parse_cfg, pred2xywhc
+from utils.util import parse_cfg, pred2xywhcc
 from models import build_model
 
 # camera shape
@@ -13,45 +15,47 @@ CAM_WIDTH, CAM_HEIGHT = 848, 480
 COLORS = [[random.randint(0, 255) for _ in range(3)] for _ in range(10)]
 
 
-def draw_bbox(img, bbox, classes):
+def draw_bbox(img, bboxs, classes):
     h, w = img.shape[0:2]
-    n = bbox.size()[0]
-    print(bbox)
+    n = bboxs.size()[0]
+    print(bboxs)
     for i in range(n):
-        p1 = (w*bbox[i, 0], h*bbox[i, 1])
-        p2 = (w*bbox[i, 2], h*bbox[i, 3])
-        class_name = classes[int(bbox[i, 4])]
-        confidence = bbox[i, 5]
-        cv2.rectangle(img, p1, p2, color=COLOR[int(bbox[i, 4])])
+        p1 = ((bboxs[i, 0]-bboxs[i, 2]/2)*w, (bboxs[i, 1]-bboxs[i, 3]/2)*h)
+        p2 = ((bboxs[i, 0]+bboxs[i, 2]/2)*w, (bboxs[i, 1]+bboxs[i, 3]/2)*h)
+        class_name = classes[int(bboxs[i, 5])]
+        confidence = bboxs[i, 4]
+        cv2.rectangle(img, p1, p2, color=COLORS[int(bboxs[i, 5])], thickness=2)
         cv2.putText(img, class_name, p1,
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[int(bboxs[i, 5])])
     return img
 
 
-def predict_img(img, model, input_size, classes):
+def predict_img(img, model, input_size):
     """get model prediction of one image
 
     Args:
         img: image ndarray
         model: pytorch trained model
+        input_size: input size
+        classes: classes
     Returns:
-        output: pytorch model output
+        xywhcc: predict image bbox
     """
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(img).convert('RGB')
+    pred_img = Image.fromarray(img).convert('RGB')
 
     transform = transforms.Compose([
         transforms.Resize((input_size, input_size)),
         transforms.ToTensor()
     ])
 
-    img = transform(img)
-    img.unsqueeze_(0)
+    pred_img = transform(pred_img)
+    pred_img.unsqueeze_(0)
 
-    pred = model(img)
+    pred = model(pred_img)[0]
     xywhcc = pred2xywhcc(pred)
-    img = draw_bbox(img, xywhcc, classes)
-    return img
+
+    return xywhcc
 
 
 def arg_parse():
@@ -63,10 +67,10 @@ def arg_parse():
     parser = argparse.ArgumentParser(
         description='YOLOv1 Pytorch Implementation')
 
-    parser.add_argument("--weight", "-w", dest='weight', default="weights/yolov1.pth",
+    parser.add_argument("--weights", "-w", dest='weights', default="weights/last.pth",
                         help="Path of model weight", type=str)
 
-    parser.add_argument("--source", "-s", dest='source', default="data/samples/test.jpg",
+    parser.add_argument("--source", "-s", dest='source', default="dataset/NWPU VHR-10 dataset/positive image set",
                         help="Path of your input file source,0 for webcam", type=str)
 
     parser.add_argument('--output', "-o", dest='output', default='output',
@@ -83,13 +87,14 @@ def arg_parse():
 
 if __name__ == "__main__":
     args = arg_parse()
-    weight_path, cfg_path, source, output, data = args.weight, args.cfg, args.source, args.output, args.data
+    weight_path, cfg_path, source, output, data = args.weights, args.cfg, args.source, args.output, args.data
 
     # load configs from config file
     cfg = parse_cfg(cfg_path)
     input_size = int(cfg['input_size'])
     data = parse_cfg(data)
-    classes = data['class_names']
+    classes = data['class_names'].split(',')
+    print('classes:', classes)
 
     # laod model
     model = build_model(weight_path, cfg)
@@ -103,9 +108,12 @@ if __name__ == "__main__":
     if source.split('.')[-1] in ['jpg', 'png', 'jpeg', 'bmp', 'tif', 'tiff', 'gif']:
         img = cv2.imread(source)
         img_name = os.path.basename(source)
-        img = predict_img(img, model, input_size, classes)
-        # save output img
-        cv2.imwrite(os.path.join(output, img_name), img)
+
+        xywhcc = predict_img(img, model, input_size)
+        if xywhcc.size()[0] != 0:
+            img = draw_bbox(img, xywhcc, classes)
+            # save output img
+            cv2.imwrite(os.path.join(output, img_name), img)
 
     # video
     elif source.split('.')[-1] in ['mp4', 'avi', 'mkv', 'flv', 'rmvb', 'mov', 'rm']:
@@ -116,7 +124,10 @@ if __name__ == "__main__":
                 print('Video load failed!')
                 break
 
-            frame = predict_img(frame, model, input_size, classes)
+            xywhcc = predict_img(frame, model, input_size)
+            if xywhcc.size()[0] != 0:
+                frame = draw_bbox(frame, xywhcc, classes)
+
             cv2.resizeWindow('frame', (int(cap.get(3)), int(cap.get(4))))
             cv2.imshow("frame", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -137,7 +148,10 @@ if __name__ == "__main__":
                 break
             print('frame shape:', frame.shape)
 
-            frame = predict_img(frame, model, input_size, classes)
+            xywhcc = predict_img(frame, model, input_size)
+            if xywhcc.size()[0] != 0:
+                frame = draw_bbox(frame, xywhcc, classes)
+
             cv2.resizeWindow('frame', (int(cap.get(3)), int(cap.get(4))))
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -152,9 +166,21 @@ if __name__ == "__main__":
             # img = cv2.imread(os.path.join(source, img_name))
             img = cv2.imdecode(np.fromfile(os.path.join(
                 source, img_name), dtype=np.uint8), cv2.IMREAD_COLOR)
-            img = predict_img(
-                img, model, input_size, classes)
+
+            # create output folder
+            output = os.path.join(output, source.split('/')[-1])
+            if os.path.exists(output):
+                os.remove(output)
+            os.makedirs(output)
+
+            # predict
+            xywhcc = predict_img(img, model, input_size)
+            if xywhcc.size()[0] != 0:
+                img = draw_bbox(img, xywhcc, classes)
+                # save output img
+                cv2.imwrite(os.path.join(output, img_name), img)
+
             print(img_name)
-            print('Class name:', class_name, 'Confidence:', str(confidence)+'%')
+            # print('Class name:', class_name, 'Confidence:', str(confidence)+'%')
             # save output img
             cv2.imwrite(os.path.join(output, img_name), img)

models/model.py
@@ -4,14 +4,22 @@ from torchvision.models import resnet34, resnet18, resnet50
 
 
 class YOLOv1(nn.Module):
+    """YOLOv1 model structure
+    yolov1 = resnet(backbon) + conv + fc
+    """
+
     def __init__(self, B, nb_classes):
         super(YOLOv1, self).__init__()
         self.B = B
         self.nb_classes = nb_classes
-        self.resnet = resnet18()
+        self.resnet = resnet18()  # you can replace to other models you like
         # print(self.resnet.fc.in_features)
-        # print(*list(self.resnet.children())[-2:])
-        self.resnet = nn.Sequential(*list(self.resnet.children())[:-2])
+        # print(*list(self.resnet.children())[-2:]) # show last two layers
+
+        # backbone part, (cut resnet's last two layers)
+        self.backbone = nn.Sequential(*list(self.resnet.children())[:-2])
+
+        # conv part
         self.conv_layers = nn.Sequential(
             nn.Conv2d(512, 1024, 3, padding=1),
             nn.BatchNorm2d(1024),
@@ -22,21 +30,32 @@ class YOLOv1(nn.Module):
             nn.Conv2d(1024, 1024, 3, padding=1),
             nn.BatchNorm2d(1024),
             nn.LeakyReLU(),
-            nn.Conv2d(1024, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(1024, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
             nn.LeakyReLU()
         )
+        # full connection part
         self.fc_layers = nn.Sequential(
-            nn.Linear(7*7*256, 1024),
+            nn.Linear(7*7*1024, 4096),
             nn.LeakyReLU(),
-            nn.Linear(1024, 7*7*(self.B*5+self.nb_classes)),
-            nn.Sigmoid()  # must add 0~1
+            nn.Linear(4096, 7*7*(self.B*5+self.nb_classes)),
+            nn.Sigmoid()  # normalized to 0~1
         )
 
     def forward(self, x):
-        out = self.resnet(x)
+        out = self.backbone(x)
         out = self.conv_layers(out)
         out = out.view(out.size()[0], -1)
         out = self.fc_layers(out)
-        out = out.reshape(-1, 7, 7, (self.B*5+self.nb_classes))
+        out = out.reshape(-1, 7, 7, self.B*5+self.nb_classes)
+        # out = out.reshape(-1, self.B*5+self.nb_classes, 7, 7) # must reshape to 30*7*7, because label is
         return out
+
+
+def build_model(weight_path, cfg):
+    model = YOLOv1(int(cfg['B']), int(cfg['nb_class']))
+
+    # load pretrained model
+    if weight_path and weight_path != '':
+        model.load_state_dict(torch.load(weight_path))
+    return model

train.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from torchvision import utils
 from torch.optim import SGD, Adam
 # from torchviz import make_dot
+from models import build_model
 from utils.util import YOLOLoss, parse_cfg
 from utils.datasets import create_dataloader
 
@@ -43,7 +44,7 @@ def train(model, train_loader, optimizer, epoch, device, train_loss_lst):
                           100. * batch_idx / len(train_loader), loss.item()))
 
     # record training loss
-    train_loss /= len(train_loader.dataset)
+    train_loss /= len(train_loader)
     train_loss_lst.append(train_loss)
     return train_loss_lst
 
@@ -61,7 +62,7 @@ def validate(model, val_loader, device, val_loss_lst):
             criterion = YOLOLoss()
             val_loss += criterion(output, target).item()
 
-    val_loss /= len(val_loader.dataset)
+    val_loss /= len(val_loader)
     print('\nVal set: Average loss: {:.4f}'.format(val_loss))
 
     # record validating loss
@@ -83,7 +84,7 @@ def test(model, test_loader, device):
             test_loss += criterion(output, target).item()
 
     # record testing loss
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(test_loader)
     print('Test set: Average loss: {:.4f}'.format(test_loss))
 
 
@@ -101,9 +102,9 @@ def arg_parse():
     parser.add_argument("--data", "-d", dest='data', default="cfg/dataset.cfg",
                         help="Your dataset config file path", type=str)
 
-    parser.add_argument("--weights", "-w", dest='weights', default="weights/yolov1.pth",
+    parser.add_argument("--weights", "-w", dest='weights', default="",
                         help="Path of pretrained weights", type=str)
-    
+
     parser.add_argument("--output", "-o", dest='output', default="output",
                         help="Output file path", type=str)
 
@@ -113,7 +114,7 @@ def arg_parse():
     parser.add_argument("--lr", "-lr", dest='lr', default=0.0001,
                         help="Training learning rate", type=float)
 
-    parser.add_argument("--batch_size", "-b", dest='batch_size', default=4,
+    parser.add_argument("--batch_size", "-b", dest='batch_size', default=32,
                         help="Training batch size", type=int)
 
     parser.add_argument("--input_size", "-i", dest='input_size', default=448,
@@ -141,7 +142,7 @@ if __name__ == "__main__":
     # device = "cpu"
 
     # build model
-    model = YOLOv1(B=2, nb_classes=10).to(device)
+    model = build_model(weights, cfg).to(device)
 
     # plot model structure
     # graph = make_dot(model(torch.rand(1, 3, input_size, input_size).cuda()),
@@ -171,7 +172,7 @@ if __name__ == "__main__":
 
     test(model, test_loader, device)
 
-    # plot loss and accuracy, save params change
+    # plot loss, save params change
     fig = plt.figure()
     plt.plot(range(epochs), train_loss_lst, 'g', label='train loss')
     plt.plot(range(epochs), val_loss_lst, 'k', label='val loss')

utils/util.py
@@ -5,66 +5,77 @@ import numpy as np
 B = 2
 NB_CLASSES = 10
 
+
 def xywhc2label(bboxs):
+    # bboxs is a xywhc list: [(x,y,w,h,c),(x,y,w,h,c)....]
     label = np.zeros((7, 7, 5*B+NB_CLASSES))
     for x, y, w, h, c in bboxs:
-        x_grid = int(x//(1/7))
-        y_grid = int(y//(1/7))
-        label[x_grid][y_grid][0:5] = np.array([x, y, w, h, 1])
-        label[x_grid][y_grid][5:10] = np.array([x, y, w, h, 1])
-        label[x_grid][y_grid][10:10+c-1] = 1
+        x_grid = int(x//(1.0/7))
+        y_grid = int(y//(1.0/7))
+        label[x_grid, y_grid, 0:5] = np.array([x, y, w, h, 1])
+        label[x_grid, y_grid, 5:10] = np.array([x, y, w, h, 1])
+        label[x_grid, y_grid, 10+c-1] = 1
     return label
 
 
 def pred2xywhcc(pred):
-    # pred is 1*7*7*(5*B+C) tensor
-    # convert to numpy array
-    # pred = pred.detach().numpy()
-    bbox = torch.zeros((7*7*2, 5+10))  # 98 * 15
+    # pred is a 7*7*(5*B+C) tensor
+    bboxs = torch.zeros((7*7*2, 5+10))  # 98*15
     for x in range(7):
         for y in range(7):
             # bbox1
-            bbox[2*(x*7+y), 0:4] = pred[x, y, 0:4]
-            bbox[2*(x*7+y), 4] = pred[x, y, 4]
-            bbox[2*(x*7+y), 5:] = pred[x, y, 10:]
+            bboxs[2*(x*7+y), 0:4] = pred[x, y, 0:4]
+            bboxs[2*(x*7+y), 4] = pred[x, y, 4]
+            bboxs[2*(x*7+y), 5:] = pred[x, y, 10:]
             # bbox2
-            bbox[2*(x*7+y)+1, 0:4] = pred[x, y, 5:9]
-            bbox[2*(x*7+y)+1, 4] = pred[x, y, 9]
-            bbox[2*(x*7+y)+1, 5:] = pred[x, y, 10:]
-    # NMS
-    xywhcc = nms(bbox)
+            bboxs[2*(x*7+y)+1, 0:4] = pred[x, y, 5:9]
+            bboxs[2*(x*7+y)+1, 4] = pred[x, y, 9]
+            bboxs[2*(x*7+y)+1, 5:] = pred[x, y, 10:]
+    # apply NMS to all bboxs
+    xywhcc = nms(bboxs)
     return xywhcc
 
 
-def nms(bbox, conf_thresh=0.1, iou_thresh=0.3):
-    # Non-Maximum Suppression
-    bbox_prob = bbox[:, 5:].clone()
-    bbox_conf = bbox[:, 4].clone().unsqueeze(1).expand_as(bbox_prob)
-    bbox_cls_spec_conf = bbox_conf*bbox_prob
+def nms(bboxs, conf_thresh=0.1, iou_thresh=0.3):
+    # Non-Maximum Suppression, bboxs is a 98*15 tensor
+    bbox_prob = bboxs[:, 5:].clone()  # 98*10
+    bbox_conf = bboxs[:, 4].clone().unsqueeze(1).expand_as(bbox_prob)  # 98*10
+    bbox_cls_spec_conf = bbox_conf*bbox_prob  # 98*10
     bbox_cls_spec_conf[bbox_cls_spec_conf <= conf_thresh] = 0
 
+    # for each class, sort the cls-spec-conf score
     for c in range(10):
-        rank = torch.sort(bbox_cls_spec_conf[:, c], descending=True).indices
+        rank = torch.sort(
+            bbox_cls_spec_conf[:, c], descending=True).indices  # sort
+        # for each bbox
         for i in range(98):
-            if bbox_cls_spec_conf[rank[i], c] != 0:
-                for j in range(i+1, 98):
-                    if bbox_cls_spec_conf[rank[j], c] != 0:
-                        iou = calculate_iou(
-                            bbox[rank[i], 0:4], bbox[rank[j], 0:4])
-                        if iou > iou_thresh:
-                            bbox_cls_spec_conf[rank[j], c] = 0
+            if bbox_cls_spec_conf[rank[i], c] == 0:
+                continue
+            for j in range(i+1, 98):
+                if bbox_cls_spec_conf[rank[j], c] != 0:
+                    iou = calculate_iou(
+                        bboxs[rank[i], 0:4], bboxs[rank[j], 0:4])
+                    if iou > iou_thresh:
+                        bbox_cls_spec_conf[rank[j], c] = 0
 
     # exclude cls-specific confidence score=0
-    bbox = bbox[torch.max(bbox_cls_spec_conf, dim=1).values > 0]
+    bboxs = bboxs[torch.max(bbox_cls_spec_conf, dim=1).values > 0]
 
     bbox_cls_spec_conf = bbox_cls_spec_conf[torch.max(
         bbox_cls_spec_conf, dim=1).values > 0]
 
-    res = torch.ones((bbox.size()[0], 6))
-    res[:, 0:4] = bbox[:, 0:4]  # bbox coord
-    res[:, 6] = torch.argmax(bbox[:, 5:], dim=1).int()  # bbox class
+    res = torch.ones((bboxs.size()[0], 6))
+
+    # return null
+    if bboxs.size[0] == 0:
+        return torch.tensor([])
+
+    # bbox coord
+    res[:, 0:4] = bboxs[:, 0:4]
     # bbox class-specific confidence scores
-    res[:, 5] = torch.max(bbox_cls_spec_conf, dim=1).values
+    res[:, 4] = torch.max(bbox_cls_spec_conf, dim=1).values
+    # bbox class
+    res[:, 5] = torch.argmax(bboxs[:, 5:], dim=1).int()
     return res
 
 
@@ -72,9 +83,9 @@ def calculate_iou(bbox1, bbox2):
     # bbox: x y w h
     bbox1, bbox2 = bbox1.cpu().detach().numpy(
     ).tolist(), bbox2.cpu().detach().numpy().tolist()
-    # print(bbox1,bbox2)
-    area1 = bbox1[2]*bbox1[3]
-    area2 = bbox2[2]*bbox2[3]
+
+    area1 = bbox1[2]*bbox1[3]  # bbox1's area
+    area2 = bbox2[2]*bbox2[3]  # bbox2's area
 
     max_left = max(bbox1[0]-bbox1[2]/2, bbox2[0]-bbox2[2]/2)
     min_right = min(bbox1[0]+bbox1[2]/2, bbox2[0]+bbox2[2]/2)
@@ -84,6 +95,7 @@ def calculate_iou(bbox1, bbox2):
     if max_left >= min_right or max_top >= min_bottom:
         return 0
     else:
+        # iou = intersect / union
         intersect = (min_right-max_left)*(min_bottom-max_top)
         return (intersect / area1+area2-intersect)
 
@@ -94,16 +106,16 @@ class YOLOLoss(nn.Module):
 
     def forward(self, preds, labels):
         batch_size = labels.size()[0]
-        # feature_map_size = labels.size()[1] # S
+
         loss_coord_xy = 0  # coord xy loss
         loss_coord_wh = 0  # coord wh loss
         loss_obj = 0  # obj loss
         loss_noobj = 0  # noobj loss
-        loss_class = 0  # classes loss
+        loss_class = 0  # class loss
+
         for n in range(batch_size):
             for x in range(7):
                 for y in range(7):
-
                     # this region has object
                     if labels[n, x, y, 4] == 1:
                         # calculate iou of two bbox
@@ -152,15 +164,16 @@ class YOLOLoss(nn.Module):
                         loss_noobj += 0.5 * \
                             torch.sum(preds[n, x, y, [4, 9]]**2)
 
+                    # end labels have object
                 # end for y
             # end for x
         # end for batchsize
 
-        # print(loss_coord_xy)
-        # print(loss_coord_wh)
-        # print(loss_obj)
-        # print(loss_noobj)
-        # print(loss_class)
+        # print('loss_coord_xy', loss_coord_xy)
+        # print('loss_coord_wh', loss_coord_wh)
+        # print('loss_coord_wh', loss_obj)
+        # print('loss_coord_wh', loss_noobj)
+        # print('loss_coord_wh', loss_class)
 
         loss = loss_coord_xy + loss_coord_wh + loss_obj + \
             loss_noobj + loss_class  # five loss terms